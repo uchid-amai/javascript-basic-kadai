212f46bab01b8096c61804f55e4a7effd4ef45dc
uchid-amai <mai836tnp@gmail.com>
2025-05-31 14:12:11 +0900

    kadai_005を提出

---
+++ kasai_005/bmi.js
@@ -0,0 +1,16 @@
+//体重の変数
+let weight;
+weight = 68
+
+//身長の変数
+let height;
+height = 1.7
+
+//BMIの変数
+let bmi;
+bmi = weight /(height * height);
+
+//コンソールへの出力
+console.log(weight);
+console.log(height);
+console.log(bmi);
